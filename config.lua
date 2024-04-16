0679490b73cf5925883fe062711fdcd56481873f
F1RST <marcandre.morrissettesoucy.1@gmail.com>
2024-04-16 00:05:18 -0400

    Initial Commit

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.Locale = GetConvar('esx:locale', 'fr')
+Config.EnableESXIdentity = true
+Config.MaxSalary = 350
+
+Config.BossGrades = { -- Uncomment and/or add additional grades you want to have access to the boss menu.
+    ['boss'] = true,
+    --['staff1'] = false,
+    --['staff2'] = false,
+    --['staff3'] = false,
+}
